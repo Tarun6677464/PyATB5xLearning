f514ecab471024201d5af6584bd2d5530852b1da
Tarun <purushtarun@gmail.com>
2025-01-12 10:05:19 +0530

    Nov9th - part2

---
+++ Nov/ex_09112024_literals_variables/Lab029_Task1.py
@@ -0,0 +1,16 @@
+#Task1
+#take 3 inputs  and perform add,sub,mul and div
+
+num1 = int(input("Enter the number1"))
+num2 = int(input("Enter the number2"))
+num3 = int(input("Enter the number3"))
+
+sum = num1 + num2 + num3
+sub = num1 - num2 - num3
+div = num1 / num2 / num3
+mul = num1 * num2 * num3
+
+print("sum of the number",sum)
+print("sub of the number",sub)
+print("div of the number",div)
+print("mul of the number",mul)
---
+++ Nov/ex_09112024_literals_variables/Lab030_Task2.py
@@ -0,0 +1,11 @@
+#Task1
+#take 2 inputs  and print reminder and quotient
+
+num1 = int(input("enter the number"))
+num2 = int(input("enter the number"))
+
+rem = num1%num2
+quo = num1//num2
+
+print("Reminder",rem)
+print("Quotient",quo)
